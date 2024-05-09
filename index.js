8b98e738c918fa90541ef8acfa4fc89d1bb86157
Naufal Al Bari <albarinaufal73@gmail.com>
2024-05-09 16:23:21 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+function togglePasswordVisibility() {
+    var passwordInput = document.getElementById('inputPassword');
+    var passwordIcon = document.querySelector('.form-control-icon');
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        passwordIcon.classList.remove('fa-eye');
+        passwordIcon.classList.add('fa-eye-slash');
+    } else {
+        passwordInput.type = 'password';
+        passwordIcon.classList.remove('fa-eye-slash');
+        passwordIcon.classList.add('fa-eye');
+    }
+}
+
+function togglePasswordVisibility(inputId) {
+    var input = document.getElementById(inputId);
+    var icon = input.nextElementSibling;
+    if (input.type === 'password') {
+        input.type = 'text';
+        icon.classList.replace('fa-eye', 'fa-eye-slash');
+    } else {
+        input.type = 'password';
+        icon.classList.replace('fa-eye-slash', 'fa-eye');
+    }
+}
